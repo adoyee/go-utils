b7e2f2f63b4749d6b9ae81d1c0583abe00c24745
wangkeen <wangkeen@hzon.com>
2020-06-04 19:37:06 +0800

    etcd

--- etcd/env.go
+++ etcd/env.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	defaultEnv  *Env
-	errNotExist = errors.New("key not exists")
+	KeyNotExist = errors.New("key not exists")
 )
 
 type Env struct {
@@ -99,10 +99,15 @@ func (env *Env) Get(ctx context.Context, prefix string, v interface{}) (err erro
 		return
 	}
 	if resp.Count == 0 {
-		return errNotExist
+		return KeyNotExist
 	}
 
 	return json.Unmarshal(resp.Kvs[0].Value, v)
+}
+
+func (env *Env) GetWithPrefix(ctx context.Context, prefix string) (resp *clientv3.GetResponse, err error) {
+	resp, err = env.cli.Get(ctx, prefix, clientv3.WithPrefix())
+	return
 }
 
 func (env *Env) Put(ctx context.Context, prefix string, v interface{}) (err error) {
@@ -115,6 +120,21 @@ func (env *Env) Put(ctx context.Context, prefix string, v interface{}) (err erro
 	return
 }
 
+func (env *Env) PutWithLease(ctx context.Context, prefix string, v interface{}) (err error) {
+	var buff []byte
+	buff, err = json.Marshal(v)
+	if err != nil {
+		return
+	}
+	_, err = env.cli.Put(ctx, prefix, string(buff), clientv3.WithLease(env.leaseID))
+	return
+}
+
+func (env *Env) Delete(ctx context.Context, prefix string, opts ...clientv3.OpOption) (err error) {
+	_, err = env.cli.Delete(ctx, prefix, opts...)
+	return
+}
+
 func (env *Env) Exist(ctx context.Context, prefix string) (exist bool, err error) {
 	var resp *clientv3.GetResponse
 	if resp, err = env.cli.Get(ctx, prefix); err != nil {
@@ -124,7 +144,7 @@ func (env *Env) Exist(ctx context.Context, prefix string) (exist bool, err error
 	return
 }
 
-func (env *Env) Lock(ctx context.Context, prefix, check string) (ok bool) {
+func (env *Env) Lock(ctx context.Context, prefix, check string) (key string, ok bool) {
 	var err error
 	mutex := concurrency.NewMutex(env.sess, prefix)
 	if err = mutex.Lock(ctx); err != nil {
@@ -132,20 +152,25 @@ func (env *Env) Lock(ctx context.Context, prefix, check string) (ok bool) {
 	}
 
 	if check == "" {
-		return true
+		return mutex.Key(), true
 	}
 
 	exist, err := env.Exist(ctx, check)
 	if err != nil {
 		_ = mutex.Unlock(ctx)
-		return false
+		return "", false
 	}
 
 	if !exist {
-		return
+		return "", false
 	}
 
-	return true
+	return mutex.Key(), true
+}
+
+func (env *Env) Unlock(ctx context.Context, key string) (err error) {
+	_, err = env.cli.Delete(ctx, key)
+	return
 }
 
 func (env *Env) CountPrefix(ctx context.Context, prefix string, opts ...clientv3.OpOption) (n int64, err error) {
@@ -169,16 +194,32 @@ func Get(ctx context.Context, prefix string, value interface{}) error {
 	return defaultEnv.Get(ctx, prefix, value)
 }
 
+func GetWithPrefix(ctx context.Context, prefix string) (*clientv3.GetResponse, error) {
+	return defaultEnv.GetWithPrefix(ctx, prefix)
+}
+
 func Put(ctx context.Context, prefix string, value interface{}) error {
 	return defaultEnv.Put(ctx, prefix, value)
+}
+
+func PutWithLease(ctx context.Context, prefix string, value interface{}) error {
+	return defaultEnv.PutWithLease(ctx, prefix, value)
 }
 
 func Exist(ctx context.Context, prefix string) (bool, error) {
 	return defaultEnv.Exist(ctx, prefix)
 }
 
-func Lock(ctx context.Context, prefix, check string) bool {
+func Lock(ctx context.Context, prefix, check string) (string, bool) {
 	return defaultEnv.Lock(ctx, prefix, check)
+}
+
+func Unlock(ctx context.Context, key string) error {
+	return defaultEnv.Unlock(ctx, key)
+}
+
+func Delete(ctx context.Context, prefix string, opts ...clientv3.OpOption) error {
+	return defaultEnv.Delete(ctx, prefix, opts...)
 }
 func CountPrefix(ctx context.Context, prefix string, opts ...clientv3.OpOption) (int64, error) {
 	return defaultEnv.CountPrefix(ctx, prefix, opts...)
