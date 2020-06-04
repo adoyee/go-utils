30f920b934f5fba0ab950de81186b2a6fa5f1d98
wangkeen <wangkeen@hzon.com>
2020-06-04 15:47:41 +0800

    test

--- etcd/env.go
+++ etcd/env.go
@@ -1,3 +1,4 @@
+// a simple wrapper of etcd
 package etcd
 
 import (
@@ -8,11 +9,11 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/clientv3/concurrency"
+	nm "github.com/coreos/etcd/clientv3/namespace"
 )
 
 var (
-	defaultEnv *Env
-
+	defaultEnv  *Env
 	errNotExist = errors.New("key not exists")
 )
 
@@ -23,16 +24,25 @@ type Env struct {
 	sess    *concurrency.Session
 }
 
-func InitEnv(ctx context.Context, cli *clientv3.Client, ttl int64) (err error) {
-	if defaultEnv != nil {
-		log.Fatal("etcd env has been initialized")
+func newEnv(ctx context.Context, ep []string, namespace string, ttl int64) (env *Env, err error) {
+	var (
+		config clientv3.Config
+		cli    *clientv3.Client
+		lease  clientv3.Lease
+		resp   *clientv3.LeaseGrantResponse
+		sess   *concurrency.Session
+	)
+
+	config.Endpoints = ep
+	config.Context = ctx
+
+	if cli, err = clientv3.New(config); err != nil {
+		return
 	}
 
-	var (
-		lease clientv3.Lease
-		resp  *clientv3.LeaseGrantResponse
-		sess  *concurrency.Session
-	)
+	cli.KV = nm.NewKV(cli.KV, namespace)
+	cli.Lease = nm.NewLease(cli.Lease, namespace)
+	cli.Watcher = nm.NewWatcher(cli.Watcher, namespace)
 
 	lease = clientv3.NewLease(cli)
 	resp, err = lease.Grant(ctx, ttl)
@@ -53,9 +63,10 @@ func InitEnv(ctx context.Context, cli *clientv3.Client, ttl int64) (err error) {
 		return
 	}
 
-	defaultEnv = &Env{
-		leaseID: resp.ID,
+	env = &Env{
+		cli:     cli,
 		lease:   lease,
+		leaseID: resp.ID,
 		sess:    sess,
 	}
 
@@ -68,24 +79,111 @@ func InitEnv(ctx context.Context, cli *clientv3.Client, ttl int64) (err error) {
 	return
 }
 
-func get(ctx context.Context, key string, v interface{}) (err error) {
-	resp, err := defaultEnv.cli.Get(ctx, key)
-	if err != nil {
-		return
+func InitEnv(ctx context.Context, ep []string, namespace string, ttl int64) (err error) {
+	if defaultEnv != nil {
+		log.Fatal("etcd env has been initialized")
 	}
 
+	var env *Env
+
+	if env, err = newEnv(ctx, ep, namespace, ttl); err != nil {
+		return
+	}
+	defaultEnv = env
+	return
+}
+
+func (env *Env) Get(ctx context.Context, prefix string, v interface{}) (err error) {
+	var resp *clientv3.GetResponse
+	if resp, err = env.cli.Get(ctx, prefix); err != nil {
+		return
+	}
 	if resp.Count == 0 {
 		return errNotExist
 	}
+
 	return json.Unmarshal(resp.Kvs[0].Value, v)
 }
 
-func put(ctx context.Context, key string, v interface{}) (err error) {
+func (env *Env) Put(ctx context.Context, prefix string, v interface{}) (err error) {
 	var buff []byte
 	buff, err = json.Marshal(v)
 	if err != nil {
 		return
 	}
-	_, err = defaultEnv.cli.Put(ctx, key, string(buff))
+	_, err = env.cli.Put(ctx, prefix, string(buff))
 	return
+}
+
+func (env *Env) Exist(ctx context.Context, prefix string) (exist bool, err error) {
+	var resp *clientv3.GetResponse
+	if resp, err = env.cli.Get(ctx, prefix); err != nil {
+		return
+	}
+	exist = resp.Count > 0
+	return
+}
+
+func (env *Env) Lock(ctx context.Context, prefix, check string) (ok bool) {
+	var err error
+	mutex := concurrency.NewMutex(env.sess, prefix)
+	if err = mutex.Lock(ctx); err != nil {
+		return
+	}
+
+	if check == "" {
+		return true
+	}
+
+	exist, err := env.Exist(ctx, check)
+	if err != nil {
+		_ = mutex.Unlock(ctx)
+		return false
+	}
+
+	if !exist {
+		return
+	}
+
+	return true
+}
+
+func (env *Env) CountPrefix(ctx context.Context, prefix string, opts ...clientv3.OpOption) (n int64, err error) {
+	var resp *clientv3.GetResponse
+
+	op := []clientv3.OpOption{clientv3.WithCountOnly()}
+	op = append(op, opts...)
+	resp, err = env.cli.Get(ctx, prefix, op...)
+	if err != nil {
+		return
+	}
+	n = resp.Count
+	return
+}
+
+func (env *Env) Watch(ctx context.Context, prefix string, opts ...clientv3.OpOption) clientv3.WatchChan {
+	return env.cli.Watch(ctx, prefix, opts...)
+}
+
+func Get(ctx context.Context, prefix string, value interface{}) error {
+	return defaultEnv.Get(ctx, prefix, value)
+}
+
+func Put(ctx context.Context, prefix string, value interface{}) error {
+	return defaultEnv.Put(ctx, prefix, value)
+}
+
+func Exist(ctx context.Context, prefix string) (bool, error) {
+	return defaultEnv.Exist(ctx, prefix)
+}
+
+func Lock(ctx context.Context, prefix, check string) bool {
+	return defaultEnv.Lock(ctx, prefix, check)
+}
+func CountPrefix(ctx context.Context, prefix string, opts ...clientv3.OpOption) (int64, error) {
+	return defaultEnv.CountPrefix(ctx, prefix, opts...)
+}
+
+func Watch(ctx context.Context, prefix string, opts ...clientv3.OpOption) clientv3.WatchChan {
+	return defaultEnv.Watch(ctx, prefix, opts...)
 }
